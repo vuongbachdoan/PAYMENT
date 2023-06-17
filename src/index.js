7e41a602eee5c0b9c3355b306f79df8faa4581df
vuongbachdoan <vuongbachdoan@gmail.com>
2023-06-17 10:45:06 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import GooglePayButton from "@google-pay/button-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GooglePayButton
+      environment="TEST"
+      paymentRequest={{
+        apiVersion: 2,
+        apiVersionMinor: 0,
+        allowedPaymentMethods: [
+          {
+            type: 'CARD',
+            parameters: {
+              allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+              allowedCardNetworks: ['MASTERCARD', 'VISA'],
+            },
+            tokenizationSpecification: {
+              type: 'PAYMENT_GATEWAY',
+              parameters: {
+                gateway: 'example',
+                gatewayMerchantId: 'exampleGatewayMerchantId',
+              },
+            },
+          },
+        ],
+        merchantInfo: {
+          merchantId: 'Rapify Cloud',
+          merchantName: 'Demo Merchant',
+        },
+        transactionInfo: {
+          totalPriceStatus: 'FINAL',
+          totalPriceLabel: 'Total',
+          totalPrice: '100.00',
+          currencyCode: 'USD',
+          countryCode: 'US',
+        },
+      }}
+      onLoadPaymentData={paymentRequest => {
+        console.log('load payment data', paymentRequest);
+      }}
+    />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
